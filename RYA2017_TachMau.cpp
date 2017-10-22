ae728ba4855da97f38d323fd825f1a117406e113
anhhungtocdo36 <loctranthanh36@gmail.com>
2017-10-22 15:32:08 +0700

    blur img

--- RYA2017_TachMau.cpp
+++ RYA2017_TachMau.cpp
@@ -18,10 +18,12 @@ void Init(){
 }
 int main(){
     Init();
-    Mat img_raw;
+    Mat img_raw, blurred_img;
     img_raw = imread("14398232797931663.bmp", CV_LOAD_IMAGE_UNCHANGED);
     while (1){
         imshow(nameWindow1,img_raw);
+        blur(img_raw, blurred_img, Size(s16_blur_min*2+1, s16_blur_min*2+1), Point(-1,-1), BORDER_DEFAULT);
+        imshow("Blur", blurred_img);
         if (waitKey(33)=='q')
             break;
     }

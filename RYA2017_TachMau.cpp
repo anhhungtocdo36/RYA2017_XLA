af2453b0beb5e3198345b45bb969f38051f47a53
anhhungtocdo36 <loctranthanh36@gmail.com>
2017-10-22 15:25:20 +0700

    Add Trackbar

--- RYA2017_TachMau.cpp
+++ RYA2017_TachMau.cpp
@@ -7,7 +7,17 @@ using namespace std;
 using namespace cv;
 
 const string nameWindow1 = "Anh Goc";
+const string nameWindow2 = "Trackbars";
+
+int s16_blur_min = 0;
+int s16_blur_max = 255;
+
+void Init(){
+    namedWindow(nameWindow2, 0);
+    createTrackbar("Blue size", nameWindow2, &s16_blur_min, s16_blur_max);
+}
 int main(){
+    Init();
     Mat img_raw;
     img_raw = imread("14398232797931663.bmp", CV_LOAD_IMAGE_UNCHANGED);
     while (1){
@@ -15,5 +25,6 @@ int main(){
         if (waitKey(33)=='q')
             break;
     }
+    
     return 0;
 }
